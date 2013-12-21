73cebbbed0dd6cb9604e08f60dd33a7632bd5535
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2013-12-21 15:10:49 -0500

    Initial commit.  Rspec installed, ready to go!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogpark::Application.config.secret_key_base = '5ab3ec5356211b812843370e20448acd51d3040f790007823e7f2607626bf806c861e53fdb2548890ede2a93d2318913067768b85f9a4028f1eb958f4ad3a5e1'
